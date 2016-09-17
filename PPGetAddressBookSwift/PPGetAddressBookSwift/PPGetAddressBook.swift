6a71b7051b44aabfea8aecee5fd6c43b63a14ca6
jkpang <jkpang@outlook.com>
2016-09-17 17:39:40 +0800

    update

--- PPGetAddressBookSwift/PPGetAddressBookSwift/PPGetAddressBook.swift
+++ PPGetAddressBookSwift/PPGetAddressBookSwift/PPGetAddressBook.swift
@@ -5,6 +5,23 @@
 //  Created by AndyPang on 16/9/16.
 //  Copyright © 2016年 AndyPang. All rights reserved.
 //
+/*
+ *********************************************************************************
+ *
+ * 如果您在使用 PPNumberButtonSwift 的过程中出现bug或有更好的建议,还请及时以下列方式联系我,我会及
+ * 时修复bug,解决问题.
+ *
+ * Weibo : CoderPang
+ * Email : jkpang@outlook.com
+ * QQ    : 2406552315
+ * GitHub: https://github.com/jkpang
+ *
+ * PS:PPGetAddressBookSwift的Objective-C版本:
+ * https://github.com/jkpang/PPGetAddressBookSwift
+ *
+ *********************************************************************************
+ */
+
 
 import Foundation
 import AddressBook
